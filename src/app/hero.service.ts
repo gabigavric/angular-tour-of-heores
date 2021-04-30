ccc01b5c60d41ddf53b3e8bf1dce268cbcfad70a
Gabrijela Gavric <gabigavric@gmail.com>
2021-04-30 16:32:56 -0700

    created message service and injected it into hero service, message service adds and clears messages

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,21 +2,15 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  /*this was for commit fa50c067e4c3fb286e848387f4abac82186ddc73 when getHeroes() was synchronous
-  getHeroes(): Hero[] {
-    return HEROES;
-  }
-  */
-
-  //simulate getting data from the server with the RxJS of() function
   getHeroes(): Observable<Hero[]> {  
     const heroes = of(HEROES); // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
     return heroes;
